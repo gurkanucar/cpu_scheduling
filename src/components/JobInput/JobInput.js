302409e3e6f39e95e304fc65c7cf3565fcc86f55
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-12-05 15:00:08 +0300

    input focus problem solved with using ref

--- src/components/JobInput/JobInput.js
+++ src/components/JobInput/JobInput.js
@@ -1,81 +1,103 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { Component, useState, useEffect, useRef } from "react";
 import { Col, Form, Button, Row, Table } from "react-bootstrap";
 
-const JobInput = (props) => {
-  const [values, setValues] = React.useState({
-    id: props.job.id,
-    jobName: props.job.jobName,
-    arrivalTime: props.job.arrivalTime,
-    burstTime: props.job.burstTime,
-    priority: props.job.priority,
-  });
-
-  const onClick = () => {
-    props.onClick(values.id);
-  };
-
-  useEffect(() => {
-    //console.log("update job input component", values);
-  }, [values]);
-
-  const handleChange = (event) => {
-    const { name, value } = event.currentTarget;
-    setValues((previousForm) => ({
-      ...previousForm,
-      [name]: value,
-    }));
-  };
-
+function MyTextInput(props) {
+  function handleChange(event) {
+    if (props.onChange) props.onChange(event);
+  }
   return (
-    <tr>
-      <td
-        style={{
-          textAlign: "center",
-          fontSize: "1.3rem",
-        }}
-      >
-        <Button variant="danger" onClick={onClick}>
-          {props?.job?.jobName}
-        </Button>
-      </td>
-      <td>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Control
-            type="number"
-            value={values.arrivalTime}
-            defaultValue={props?.job?.arrivalTime ? props?.job?.arrivalTime : 0}
-            name="arrivalTime"
-            onChange={handleChange}
-            placeholder="Arrival Time"
-          />
-        </Form.Group>
-      </td>
-      <td>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Control
-            type="number"
-            value={values.burstTime}
-            defaultValue={props?.job?.burstTime ? props?.job?.burstTime : 0}
-            name="burstTime"
-            onChange={handleChange}
-            placeholder="Burst Time"
-          />
-        </Form.Group>
-      </td>
-      <td>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Control
-            type="number"
-            value={values.priority}
-            defaultValue={props?.job?.priority ? props?.job?.priority : 0}
-            name="priority"
-            onChange={handleChange}
-            placeholder="Priority"
-          />
-        </Form.Group>
-      </td>
-    </tr>
+    <Form.Group controlId="formBasicPassword">
+      <Form.Control
+        type="number"
+        ref={props.inputRef}
+        onChange={handleChange}
+        value={props.value}
+        // defaultValue={
+        //   this.props?.job?.burstTime ? this.props?.job?.burstTime : 0
+        // }
+        name={props.name}
+        //onChange={this.handleChange}
+        placeholder={props.placeholder}
+      />
+    </Form.Group>
+    //  <p>
+    //      <input type='email' value={props.value} name={props.name} ref={props.inputRef} onChange={handleChange} />
+    //  </p>
   );
-};
+}
+
+class JobInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.job.id,
+      jobName: this.props.job.jobName,
+      arrivalTime: this.props.job.arrivalTime,
+      burstTime: this.props.job.burstTime,
+      priority: this.props.job.priority,
+    };
+
+    this.arrivalTimeRef = React.createRef();
+    this.burstTimeRef = React.createRef();
+    this.priorityRef = React.createRef();
+  }
+
+  onClick = () => {
+    this.props.onClick(this.state.id);
+  };
+
+  handleInputChange = (event) => {
+    //console.log(event.target.value);
+    event.preventDefault();
+    this.props.inputChangeHandler({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    });
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <tr>
+        <td
+          style={{
+            textAlign: "center",
+            fontSize: "1.3rem",
+          }}
+        >
+          <Button variant="danger" onClick={this.onClick}>
+            {this.props?.job?.jobName}
+          </Button>
+        </td>
+        <td>
+          <MyTextInput
+            onChange={this.handleInputChange}
+            name="arrivalTime"
+            placeholder="arrival time"
+            value={this.state.arrivalTime}
+          />
+        </td>
+        <td>
+          <MyTextInput
+            onChange={this.handleInputChange}
+            name="burstTime"
+            placeholder="burst time"
+            value={this.state.burstTime}
+          />
+        </td>
+        <td>
+          <MyTextInput
+            onChange={this.handleInputChange}
+            name="priority"
+            placeholder="priority"
+            value={this.state.priority}
+          />
+        </td>
+      </tr>
+    );
+  }
+}
 
 export default JobInput;
